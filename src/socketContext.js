ec9ee5ac2201575162b1fc214fac8fc9aa655585
jstgrowup <deysubham999@gmail.com>
2022-11-08 23:26:41 +0530

    added

--- src/socketContext.js
+++ src/socketContext.js
@@ -20,8 +20,10 @@ const ContextProvider = ({ children }) => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((currentStream) => {
                 setStream(currentStream);
-                
-                myVideo.current.srcObject = currentStream;
+                if (myVideo.current) {
+
+                    myVideo.current.srcObject = currentStream;
+                }
             });
 
         socket.on('me', (id) => setMe(id));
